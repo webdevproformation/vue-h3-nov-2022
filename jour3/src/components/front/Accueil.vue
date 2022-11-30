12dd790a521aa9fd5a22cdbe9ba6d43aa432c78e
Malik <malik.h@webdevpro.net>
2022-11-30 11:34:07 +0100

    enoncé

--- jour3/src/components/front/Accueil.vue
+++ jour3/src/components/front/Accueil.vue
@@ -1,11 +1,50 @@
 <template>
     <div>
         <h1>Bienvenu</h1>
+        <!-- <pre>
+            {{ JSON.stringify(articles , null , " ") }}
+        </pre> -->
+        <div class="row">
+            <article v-for="article , index in articles" :key="index" class="col-3">
+                <h2>{{ article.title }}</h2>
+                <p>{{ article.body }}</p>
+                <RouterLink :to="'/'" class="btn btn-primary">lire la suite ...</RouterLink> 
+            </article>
+        </div>
     </div>
 </template>
+<script setup>
+// équivalent avec API Composition 
+import { ref , onMounted } from "vue"
+import { RouterLink } from "vue-router"
 
-<script>
-    export default {
-        
-    }
+let articles = ref([]);
+//let articles = reactive([]);
+
+onMounted( () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(reponse => reponse.json())
+            .then(data => articles.value = data)
+} )
+
 </script>
+
+<!-- <script>
+// api options
+    import { RouterLink } from "vue-router"
+    export default {
+        data : () => {
+            return {
+                articles : []
+            }
+        },
+        components : {
+            "RouterLink" : RouterLink
+        },  
+        mounted : function(){
+            fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(reponse => reponse.json())
+            .then(data => this.articles = data)
+        }
+    }
+</script> -->
