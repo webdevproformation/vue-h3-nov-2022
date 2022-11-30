713492a5c4d3b0e3c16b78952b5f4967119649eb
Malik <malik.h@webdevpro.net>
2022-11-30 11:06:58 +0100

    cas pratique

---
+++ jour3-note/exemple-suite.js
@@ -0,0 +1,4 @@
+/* import b from "./exemple"
+import {a , C} from "./exemple"  */
+
+import b , {a , C} from "./exemple" ;
---
+++ jour3-note/exemple.js
@@ -0,0 +1,8 @@
+// 1 seul export default 
+// plusieurs export 
+
+export const a = 10 ;
+
+export default function b () {}
+
+export class C{}
