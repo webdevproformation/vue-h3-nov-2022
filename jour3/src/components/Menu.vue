98338a50c7660c9a774462213b83f4b9997036c6
Malik <malik.h@webdevpro.net>
2022-11-30 10:30:46 +0100

    SPA

--- jour3/src/components/Menu.vue
+++ jour3/src/components/Menu.vue
@@ -1,20 +1,24 @@
 <template>
     <div class="bg-warning">
         <nav class="navbar navbar-expand navbar-light container">
-            <a href="#" class="navbar-brand">Router jour3</a>
+            <RouterLink :to="'/'" class="navbar-brand">Router jour3</RouterLink>
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a href="/" class="nav-link">Accueil</a>
+                    <RouterLink :to="'/'" class="nav-link">Accueil</RouterLink>
                 </li>
                 <li class="nav-item">
-                    <a href="/contact" class="nav-link">Contact</a>
+                    <RouterLink :to="{name:'contact'}" class="nav-link">Contact</RouterLink>
                 </li>
             </ul>
         </nav>
     </div>
 </template>
 <script>
+import { RouterLink } from "vue-router"
     export default {
-        
+        name : "menu",
+        components : {
+            "RouterLink" : RouterLink
+        }
     }
 </script>
