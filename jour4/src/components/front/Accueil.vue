0dd03c0e2e4e90b03c30669c65e117869d3d6634
Malik <malik.h@webdevpro.net>
2022-12-01 10:53:01 +0100

    correction

--- jour4/src/components/front/Accueil.vue
+++ jour4/src/components/front/Accueil.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
         <h1>Accueil</h1>
+        <Exo1>
+            <template #header>
+                <h1>Exo 1</h1>
+            </template>
+            <template #body>
+                <select>
+                    <option value="">France</option>
+                    <option value="">Belgique</option>
+                    <option value="">Allemagne</option>
+                </select>
+            </template>
+            <template #footer>
+                <a href="https://google.fr">aller sur google</a>
+            </template>
+        </Exo1>
         <Enfant :text="'premier slot'">
             <h2>je suis le premier enfant</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, assumenda sunt! Saepe soluta asperiores maxime aut perspiciatis necessitatibus tempora similique placeat tempore repellat? Tenetur, porro! Harum consectetur doloribus nihil maxime.</p>
@@ -17,6 +32,7 @@
 
 <script>
 import Enfant from './Enfant.vue';
+import Exo1 from './Exo1.vue';
 
     export default {
     data : () => {
@@ -29,6 +45,6 @@ import Enfant from './Enfant.vue';
             alert("je suis le parent")
         }
     },
-    components: { Enfant }
+    components: { Enfant, Exo1 }
 }
 </script>
---
+++ jour4/src/components/front/Exo1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <header>
+            <slot name="header"></slot>
+            <button>action</button>
+        </header>
+        <main>
+            <slot name="body"></slot>
+        </main>
+        <footer>
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
