2a789376b522328486b24f99e8821e4009060a96
Malik <malik.h@webdevpro.net>
2022-12-01 09:51:59 +0100

    un message slot

--- jour4/src/components/front/Accueil.vue
+++ jour4/src/components/front/Accueil.vue
@@ -1,11 +1,34 @@
 <template>
     <div>
         <h1>Accueil</h1>
+        <Enfant :text="'premier slot'">
+            <h2>je suis le premier enfant</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, assumenda sunt! Saepe soluta asperiores maxime aut perspiciatis necessitatibus tempora similique placeat tempore repellat? Tenetur, porro! Harum consectetur doloribus nihil maxime.</p>
+        </Enfant>
+        <Enfant :text="'deuxième slot'">
+            <h2>autre enfant</h2>
+            <img src="https://via.placeholder.com/200x100" alt="">
+        </Enfant>
+        <Enfant :text="'slot avec bouton'">
+            <button @click="action" class="btn btn-outline-danger">{{nom}}</button>
+        </Enfant>
     </div>
 </template>
 
 <script>
+import Enfant from './Enfant.vue';
+
     export default {
-        
-    }
+    data : () => {
+        return {
+            nom : "action parent"
+        }
+    },
+    methods : {
+        action : function(){
+            alert("je suis le parent")
+        }
+    },
+    components: { Enfant }
+}
 </script>
---
+++ jour4/src/components/front/Enfant.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <hr>
+        <p>{{ text }}</p>
+        <slot />
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {
+        data : () => {
+        return {
+            nom : "action enfant"
+        }
+        },
+        methods : {
+            action : function(){
+                alert("je suis l'enfant")
+            }
+        },
+        props : {text : String}
+    }
+</script>
