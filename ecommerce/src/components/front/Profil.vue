ce4dc740fa1183515285544c85ea0476e305b289
Malik <malik.h@webdevpro.net>
2022-12-02 16:05:08 +0100

    cas pratique

--- ecommerce/src/components/front/Profil.vue
+++ ecommerce/src/components/front/Profil.vue
@@ -1,3 +1,11 @@
 <template>
     <h1>Profil</h1>
+    <!--
+        créer un tableau qui va lister l'ensemble de commandes réalisées par le client qui est connecté
+        #num commande 	
+        nombre de produit	
+        montant commande 	
+        date commande 	
+        status
+    -->
 </template>
