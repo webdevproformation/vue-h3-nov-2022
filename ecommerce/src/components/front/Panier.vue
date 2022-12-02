5bc326f4ce5e696b77279decb8fe684fb409fc11
Malik <malik.h@webdevpro.net>
2022-12-02 09:54:47 +0100

    start panier

--- ecommerce/src/components/front/Panier.vue
+++ ecommerce/src/components/front/Panier.vue
@@ -1,3 +1,9 @@
 <template>
     <h1>votre Panier</h1>
+    <TableauPanier />
 </template>
+<script setup>
+import TableauPanier from './TableauPanier.vue';
+
+
+</script>
---
+++ ecommerce/src/components/front/TableauPanier.vue
@@ -0,0 +1,36 @@
+<template>
+    <table class="table table-striped table-hover align-middle">
+        <thead>
+            <tr>
+                <th>#id</th>
+                <th>nom</th>
+                <th>image</th>
+                <th>prix</th>
+                <th>quantite</th>
+                <th>total</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="produit , index in panierStore.panier" :key="index">
+                <td>{{ produit.id }}</td>
+                <td>{{ produit.nom }}</td>
+                <td><img v-bind:src="produit.image" alt="" width="150"></td>
+                <td>{{produit.prix}}</td>
+                <td>
+                    <span class="d-flex align-items-baseline">
+                        <button class="btn btn-dark btn-sm" @click="moins(produit.id)">-</button>
+                        <span class="mx-3">{{produit.quantite}}</span>
+                        <button class="btn btn-dark btn-sm" @click="plus(produit.id)">+</button>
+                    </span>
+                </td>
+                <td>{{ produit.prix * produit.quantite }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+    import { usePanierStore } from "../../stores/panierStore"
+    let panierStore  =  usePanierStore()
+    console.log(panierStore.panier);
+</script>
